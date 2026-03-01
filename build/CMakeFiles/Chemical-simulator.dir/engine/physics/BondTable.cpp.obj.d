42b259a9024419f1e292edd1b3b9a7a1d3ba6190
ElectroZybr <electrozybr@gmail.com>
2026-03-01 13:46:17 +0300

    Add .gitignore and stop tracking build artifacts

--- build/CMakeFiles/Chemical-simulator.dir/engine/physics/BondTable.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Chemical-simulator.dir/engine/physics/BondTable.cpp.obj: \
- C:\Users\elect\Documents\Coding\Chemical-simulator\engine\physics\BondTable.cpp \
- C:\Users\elect\Documents\Coding\Chemical-simulator\engine\physics\BondTable.h
